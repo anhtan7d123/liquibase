4d7b128e4a2732299c93ba74c0613c9c7d42db94
anhtan7d123 <tanltn123456@gmail.com>
2023-04-21 17:33:45 +0700

    refactor

--- src/main/java/com/example/liquibase/LiquibaseApplication.java
+++ src/main/java/com/example/liquibase/LiquibaseApplication.java
@@ -6,7 +6,6 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
@@ -16,24 +15,15 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class LiquibaseApplication {
 
-//	public static void main(String[] args) throws SQLException, LiquibaseException {
-//		Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/liquibase",
-//					"postgres", "05042001aA");
-//
-//		Database database = DatabaseFactory.getInstance()
-//					.findCorrectDatabaseImplementation(new JdbcConnection(c));
-//
-//		Liquibase liquibase = new Liquibase("classpath:master-changelog.xml", new ClassLoaderResourceAccessor(), database);
-//	}
 	public static void main(String[] args) throws SQLException, LiquibaseException {
-		Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/liquibase",
+		Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/liquibase1",
 					"postgres", "05042001aA");
 
 		Database database = DatabaseFactory.getInstance()
 					.findCorrectDatabaseImplementation(new JdbcConnection(c));
 
-		Liquibase liquibase = new Liquibase("classpath:db-changelog.xml", new ClassLoaderResourceAccessor(), database);
+		Liquibase liquibase = new Liquibase("classpath:database/migration/master-changelog.xml", new ClassLoaderResourceAccessor(), database);
 
-		liquibase.update("");
+		liquibase.update("test");
 	}
 }
